7339444ad4f2d5517d2c3cb1782147a4a43172c9
Maks <mshevkun.dev@gmail.com>
2023-10-04 12:36:01 -0400

    Changed color palettes, smal improvements

--- js/slider/demo3.js
+++ js/slider/demo3.js
@@ -135,23 +135,22 @@
       let startX, endX;
       const self = this;
 
-
       const slidesArr = [...document.querySelectorAll(".slide")];
       slidesArr.forEach((slide) => {
         slide.addEventListener("touchstart", function (e) {
           startX = e.touches[0].clientX;
         });
-        slide.addEventListener("touchstart", function (e) {
-          startX = e.touches[0].clientX;
-        });
+        // slide.addEventListener("touchstart", function (e) {
+        //   startX = e.touches[0].clientX;
+        // });
         slide.addEventListener("touchend", function (e) {
           endX = e.changedTouches[0].clientX;
 
-          if (startX - endX > 50 & window.innerWidth <= 768) {
+          if ((startX - endX > 50) & (window.innerWidth <= 768)) {
             console.log(window.innerWidth);
             // Swipe left, show next slide
             self.navigate("next");
-          } else if (endX - startX > 50 & window.innerWidth <= 768) {
+          } else if ((endX - startX > 50) & (window.innerWidth <= 768)) {
             // Swipe right, show previous slide
             self.navigate("prev");
           }
@@ -180,9 +179,13 @@
           this.navigate("next");
         }
       });
+      // Start auto sliding
+      this.startAutoSlide();
     }
     navigate(dir = "next") {
       if (this.isAnimating) return false;
+      // Stop auto sliding
+      this.stopAutoSlide();
       this.isAnimating = true;
 
       const animateShapeInTimeline = anime.timeline({
@@ -298,6 +301,14 @@
       };
 
       animateShapeInTimeline.finished.then(animateSlides).then(animateShapeOut);
+    }
+    startAutoSlide() {
+      this.autoSlideInterval = setInterval(() => {
+        this.navigate("next");
+      }, 1000);
+    }
+    stopAutoSlide() {
+      clearInterval(this.autoSlideInterval);
     }
   }
 
